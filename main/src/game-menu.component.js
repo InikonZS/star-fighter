b9eb1310ad96d090df25c21993b353afa8601d74
Inikon <ZhanZS@mail.ru>
2020-06-16 23:05:35 +0300

    refactor: wrap multipage menu in Pager class

--- main/src/game-menu.component.js
+++ main/src/game-menu.component.js
@@ -1,4 +1,5 @@
 const Control = require('./control-js/control.component.js');
+const Pager = require('./control-js/pager.component.js');
 
 class GameMenu extends Control{
   constructor(parentNode, glCanvas){
@@ -7,55 +8,68 @@ class GameMenu extends Control{
     });
     this.glCanvas = glCanvas;
     //this.isActive = true;
-    this.background = new Control(this.node, 'div', 'menu_background');
+    this.menu = new Pager(this.node, '', 'menu_background');
 
-    this.startButton = new Control(this.background.node, 'div', 'menu_item', 'start new',()=>{
-      this.background2.show();
-      this.background.hide();
-      this.background3.hide();
-     // this.deactivate();
+    this.mainMenu = this.menu.addPage('Main Menu');
+    this.missionMenu = this.menu.addPage('Select Mission');
+    this.gameMenu = this.menu.addPage('Paused');
+    this.gameOverMenu = this.menu.addPage('Game Over');
+    this.menu.selectPage(this.mainMenu);
+
+    this.startButton = new Control(this.mainMenu.node, 'div', 'menu_item', 'start new',()=>{
+      this.menu.selectPage(this.missionMenu);
     });
 
 
     ////
-    this.background2 = new Control(this.node, 'div', 'menu_background');
-    this.background2.hide();
-    this.m1Button = new Control(this.background2.node, 'div', 'menu_item', 'mission1',()=>{
+    //this.background2 = new Control(this.node, 'div', 'menu_background');
+    //this.background2.hide();
+    this.m1Button = new Control(this.missionMenu.node, 'div', 'menu_item', 'mission1',()=>{
       this.glCanvas.start();
       this.glCanvas.game.loadMission('1');
       this.isActive = false;
       this.refresh();
-      this.background3.show();
-      this.background2.hide();
-      this.background.hide();
+      this.menu.selectPage(this.gameMenu);
+     // this.background3.show();
+      //this.background2.hide();
+     // this.background.hide();
       //this.deactivate();
     });
 
-    this.m2Button = new Control(this.background2.node, 'div', 'menu_item', 'mission2',()=>{
+    this.m2Button = new Control(this.missionMenu.node, 'div', 'menu_item', 'mission2',()=>{
       this.glCanvas.start();
       this.glCanvas.game.loadMission('');
       this.isActive = false;
       this.refresh();
-      this.background3.show();
+      this.menu.selectPage(this.gameMenu);
+     /* this.background3.show();
       this.background2.hide();
-      this.background.hide();
+      this.background.hide();*/
     });
 
-
-    ///3
-    this.background3 = new Control(this.node, 'div', 'menu_background');
-    this.resumeButton = new Control(this.background3.node, 'div', 'menu_item', 'continue',()=>{
-      this.deactivate();
-    });
-    this.mainMenuButton = new Control(this.background3.node, 'div', 'menu_item', 'to main menu',()=>{
-      this.background.show();
-      this.background3.hide();
-      this.background2.hide();
+    this.mainMenuButtonM = new Control(this.missionMenu.node, 'div', 'menu_item', 'to main menu',()=>{
+      this.menu.selectPage(this.mainMenu);
+     // this.background.show();
+     // this.background3.hide();
+     // this.background2.hide();
       //this.deactivate();
     });
-    this.background2.hide();
-      this.background.show();
-      this.background3.hide();
+
+    ///3
+    //this.background3 = new Control(this.node, 'div', 'menu_background');
+    this.resumeButton = new Control(this.gameMenu.node, 'div', 'menu_item', 'continue',()=>{
+      this.deactivate();
+    });
+    this.mainMenuButton = new Control(this.gameMenu.node, 'div', 'menu_item', 'to main menu',()=>{
+      this.menu.selectPage(this.mainMenu);
+     // this.background.show();
+     // this.background3.hide();
+     // this.background2.hide();
+      //this.deactivate();
+    });
+  //  this.background2.hide();
+  //    this.background.show();
+  //    this.background3.hide();
     this.refresh();
   }
 
