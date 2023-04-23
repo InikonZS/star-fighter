cea9326f5d4806129a1ef99d91accf8d18b35fde
inikonzs <zhanzs@mail.ru>
2023-04-23 17:45:32 +0200

    update webpack to 5

--- main/src/index.js
+++ main/src/index.js
@@ -1,4 +1,9 @@
 const {App} = require('./app.js');
+import "./styles/style_2.css";
+import "./styles/gamemenu.css";
+import "./styles/bar-indicator.css";
+import "./styles/game-screen.css";
+import "./styles/gm-screen.css";
 
 const mainNode = document.querySelector('#app-main');
 
